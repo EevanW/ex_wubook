f02a48738bdcdb66b58d610e206d04e6c49dc1c9
Ivan Ivchenko <waltereevan@gmail.com>
2019-09-04 12:53:33 +0600

    Upgrade version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExWubook.MixProject do
   def project do
     [
       app: :ex_wubook,
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
